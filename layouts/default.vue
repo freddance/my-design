caae19363c057645af311be0864fb6c4629d78f4
freddance <70547931+freddance@users.noreply.github.com>
2022-09-15 11:27:44 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <LoadingBar />
+    <Header />
+    <Nuxt  />
+<!--    <Footer />-->
+  </div>
+</template>
+
+<script>
+
+import Header from "../components/Header";
+import LoadingBar from "../components/LoadingBar";
+import Footer from "../components/Footer";
+export default  {
+  components: {Header, Footer, LoadingBar}
+}
+</script>
